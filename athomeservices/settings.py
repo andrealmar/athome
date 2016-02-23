7ac87e62bef0cac0df5729213f1b8d37b5ade09f
Andre Almar <andre@y7mail.com>
2016-02-23 17:44:40 -0300

    added dj_database_url configs

--- athomeservices/settings.py
+++ athomeservices/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 from decouple import config
+from dj_database_url import parse as dburl
 from whitenoise import WhiteNoise
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -77,11 +78,10 @@ WSGI_APPLICATION = 'athomeservices.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3 ')
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
 }
 
 
